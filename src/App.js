d090e6ba1854370974b282d00d7ab88410d47ad1
juniordugue <jrdugue@gmail.com>
2021-01-17 15:02:19 -0500

    bug fixed: fixed issues with css not updating, created empty components after reinstalling npm pkg and reinstalling tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
-    <div className="container mx-auto">
-      <header className="bg-gray-500">
-        <h1 className="text-lg	">Yoooo</h1>
-        <p className="text-red-700">This is a test</p>
-      </header>
-    </div>
+   <>
+     <Navbar/>
+   </>
   );
 }
 
