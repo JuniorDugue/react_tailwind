4220022fc3b6dfe49048c24b9f84ee0761a1b573
juniordugue <jrdugue@gmail.com>
2021-01-17 15:19:49 -0500

    add: created mobile/desktop menu in navbar component, then responsive styling to the hero component and then imported them both in app.js

---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+  <div className="bg-white h-screen flex flex-col justify-center items-center">
+    <h2 className="h1 lg:text-9xl md:text-7xl sm:text-5xl text-3xl font-black mb-14">
+      TailwindCSS
+    </h2>
+  </div>
+  )
+}
+
+export default Hero
